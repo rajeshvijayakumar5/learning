421cc2970ce71b1d9cdd11d2aa06e4ea0b5e59d1
Rajesh <rajeshvijayakumar5@gmail.com>
2024-11-03 18:05:01 +0530

    Greetings test message added

--- javalearning/src/main/java/com/rk/samples/HelloWorld.java
+++ javalearning/src/main/java/com/rk/samples/HelloWorld.java
@@ -11,6 +11,6 @@ package com.rk.samples;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello GitHub!");
+        System.out.println("Hello GitHub connection testing!");
     }
 }
