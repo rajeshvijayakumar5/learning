f1b15afc55a787642708ae27bbd4363c60e15da5
Rajesh <rajeshvijayakumar5@gmail.com>
2024-11-03 18:36:14 +0530

    Very First commit from netbeans after cloning

---
+++ javalearning/src/main/java/com/rk/samples/Employee.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rk.samples;
+
+/**
+ *
+ * @author rajes
+ */
+public class Employee {
+    int id;
+    String name;
+    int age;
+    double saiary;
+}
