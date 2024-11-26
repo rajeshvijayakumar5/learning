2b75f179c65ebe2549276261b932d2b8126d354b
Rajesh <rajeshvijayakumar5@gmail.com>
2024-11-27 01:06:22 +0530

    Adding method Hiding

---
+++ javalearning/src/main/java/com/rk/samples/MethodHiding.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rk.samples;
+
+/**
+ *
+ * @author rajes
+ */
+class BaseMH {
+
+    public static void print() {
+        System.out.println("HELLO");
+    }
+}
+
+class DerivedMH extends BaseMH {
+
+    public static void print() {
+        System.out.println("WELCOME");
+    }
+}
+
+public class MethodHiding {
+
+    public static void main(String[] args) {
+        BaseMH base = new DerivedMH();                                  // Accessing static method print
+        base.print();
+        DerivedMH derived = new DerivedMH();
+        derived.print();                                                // Accessing static method print
+    }
+}
