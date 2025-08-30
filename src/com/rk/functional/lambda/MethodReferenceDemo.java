16ac4ad1aa87db15c5c6210b0594610adc85a325
Rajesh <rajeshvijayakumar5@gmail.com>
2025-08-30 19:28:02 +0530

    Rename classes

--- src/com/rk/functional/lambda/MethodReferenceDemo.java
+++ src/com/rk/functional/lambda/MethodReferenceDemo.java
@@ -3,12 +3,12 @@ package com.rk.functional.lambda;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-class MyClass{
+class Employee{
     private final String firstName;
     private final String lastName;
     private final int age;
 
-    public MyClass(String firstName, String lastName,  int age){
+    public Employee(String firstName, String lastName, int age){
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
@@ -37,23 +37,23 @@ interface TriFunction<T,U,R,S>{
     S apply(T t, U u, R r);
 }
 
-public class MethodReferenceExample {
+public class MethodReferenceDemo {
     public static void main(String[] args) {
         //Type 1 method reference
-        MyClass myClass = new MyClass("","",0);
-        Supplier<Integer> supplier = myClass::getAge;
+        Employee employee = new Employee("Will","Smith",30);
+        Supplier<Integer> supplier = employee::getAge;
         System.out.println(supplier.get());
 
         //Type 2 Method Reference
-        Consumer<String> consumer = MyClass::displayFirstName;
+        Consumer<String> consumer = Employee::displayFirstName;
         consumer.accept("Tom");
 
         //Type 3 Method reference
-        TriFunction<MyClass, String, String, String> triFunction = MyClass::getFullName;
-        System.out.println(triFunction.apply(new MyClass("","",0),"Tom", "Cruise"));
+        TriFunction<Employee, String, String, String> triFunction = Employee::getFullName;
+        System.out.println(triFunction.apply(new Employee("","",0),"Tom", "Cruise"));
 
         //Type 4 Method Reference: Constructor
-        TriFunction<String, String, Integer, MyClass> myFunction = MyClass::new;
+        TriFunction<String, String, Integer, Employee> myFunction = Employee::new;
         System.out.println(myFunction.apply("Tom", "Cruise", 25));
     }
 }
