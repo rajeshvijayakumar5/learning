770c7478467b20103c5d6ec49179890c268252cc
Rajesh <rajeshvijayakumar5@gmail.com>
2025-09-01 23:14:31 +0530

    Add demo program showcasing the quickest ways to create Streams in Java

---
+++ src/com/rk/functional/lambda/StreamDemo.java
@@ -0,0 +1,36 @@
+package com.rk.functional.lambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamDemo {
+    public static void main(String[] args) {
+        // 1. creating streams from collections
+        List<String> alphabets = new ArrayList<>();
+        alphabets.add("a");
+        alphabets.add("b");
+        alphabets.add("c");
+        alphabets.add("d");
+        alphabets.stream().forEach(System.out::println);
+
+        // 2. creating Streams using Arrays static method
+        Arrays.stream(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}).forEach(System.out::println);
+
+        // 3. creating Streams using static method
+        Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9).forEach(System.out::println);
+
+        // 4. building stream using Stream.Builder
+        Stream.Builder<Integer> streamBuilder = Stream.builder();
+        streamBuilder.add(1);
+        streamBuilder.add(2);
+        streamBuilder.add(3);
+        streamBuilder.add(4);
+        Stream<Integer> streamBuilt = streamBuilder.build();
+        streamBuilt.forEach(System.out::println);
+
+        // 5. using iterate and limit method
+        Stream.iterate(0, i -> i + 1).limit(100).forEach(System.out::println);
+    }
+}
